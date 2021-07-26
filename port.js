9c1fe13308d204e4e8971f71eac200d2e1a7de63
maaamny <s.manami66@gmail.com>
2021-07-26 09:58:26 +0900

    変更2

--- port.js
+++ port.js
@@ -4,20 +4,20 @@ var text = document.getElementById("text");
 document.getElementById("photo-1")
         .addEventListener("click", function() {
     photo.src = "img4.jpg";
-    text.innerHTML = "マーブル柄のベンガルちゃん";
+    text.innerHTML = "2021/6/28 マーブル柄";
 });
 document.getElementById("photo-2")
         .addEventListener("click", function() {
     photo.src = "img5.jpg";
-    text.innerHTML = "眠そうなベンガル";
+    text.innerHTML = "2021/6/29 マーブル柄";
 });
 document.getElementById("photo-3")
         .addEventListener("click", function() {
     photo.src = "img6.jpg";
-    text.innerHTML = "ミルクを飲むベンガル";
+    text.innerHTML = "2021/6/29 スポテッド柄";
 });
 document.getElementById("photo-4")
         .addEventListener("click", function() {
     photo.src = "img7.jpg";
-    text.innerHTML = "スポテッド柄のベンガルちゃん";
+    text.innerHTML = "2021/6/30 スポテッド柄";
 });
